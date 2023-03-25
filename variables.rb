637562c66e17c87299c08fce17a6b09638628100
Dylan Riesenman <djriesenman@gmail.com>
2023-03-24 19:01:00 -0600

    Better puts statements and fix variable reassignment

--- variables.rb
+++ variables.rb
@@ -1,23 +1,25 @@
 name = "Dylan"
 last_name = "Riesenman"
 dog_name = "Duke"
+
+name = "Bill"
+last_name = "Johnson"
+dog_name = "Max"
+
 age = 29
 dog_age = 8
 favorite_number = 16
+
 pi = 3.14
 half_of_mortgage = 1436.26
 avg_human_temp = 98.6
+
 sky_is_blue = true
 in_colorado = true
 car_is_yellow = false
 
-puts "Name: #{name}"
-puts "Last name: #{last_name}"
-puts "Dog name: #{dog_name}"
-puts "Age: #{age}"
+puts "My name is #{name}" 
+puts "My last name is #{last_name}"
+puts "My dog's name is #{dog_name}"
+puts "My age is #{age}"
 
-
-# Reassigning variables
-name = "Bill"
-last_name = "Wallace"
-dog_name = "Joe"
