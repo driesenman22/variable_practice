e6753acb6a8f463b1bab2e87913fd88f3b281c30
Dylan Riesenman <djriesenman@gmail.com>
2023-03-24 18:42:03 -0600

    puts statments and reassigning variables

--- variables.rb
+++ variables.rb
@@ -10,3 +10,14 @@ avg_human_temp = 98.6
 sky_is_blue = true
 in_colorado = true
 car_is_yellow = false
+
+puts "Name: #{name}"
+puts "Last name: #{last_name}"
+puts "Dog name: #{dog_name}"
+puts "Age: #{age}"
+
+
+# Reassigning variables
+name = "Bill"
+last_name = "Wallace"
+dog_name = "Joe"
